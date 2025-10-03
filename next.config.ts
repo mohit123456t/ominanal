77c4c7f9bd54076d42d84e7f85089d577d8d59a1
jai minesh <jaiminesh293@gmail.com>
2025-10-03 17:48:23 +0000

    Error: Invalid src prop (https://www.pexels.com/download/video/29415106/

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pexels.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
